8abb0b3a3c5b0281217f829e9afe778be4dd5f79
TianYunZi <zxysegh@gmail.com>
2018-03-07 00:21:22 +0800

    Initial commit

---
+++ 1.1.1.spring-cloud-eureka-practise/src/main/java/org/eureka1/practise/EurekaApplication1.java
@@ -0,0 +1,14 @@
+package org.eureka1.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication1.class, args);
+    }
+}
